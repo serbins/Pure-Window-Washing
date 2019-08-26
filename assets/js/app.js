0a283587840a1fe8cfe238d86a41c38cf318f358
steveserbins <steveserbins@icloud.com>
2019-08-26 15:09:09 -0600

    Addded css and js code
    Partially styled up header/about me/ qoute sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,9 @@
 /* Mobile Navigation */
 
 /* Qoute App */
+document.getElementById('qouteOutput').style.visibility = 'hidden';
+document.getElementById('windowQty').addEventListener('input', function(e){
+document.getElementById('qouteOutput').style.visibility = 'visible';
+var windowQty = e.target.value;
+document.getElementById('qoute').innerHTML = '$' + windowQty*7;
+});
